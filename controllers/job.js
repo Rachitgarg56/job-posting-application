dd52a1e1504b302298a0f710edcc1909fbbf5382
Rachit Garg <rachitgarg56@gmail.com>
2024-05-19 15:23:23 +0530

    error handling done

--- controllers/job.js
+++ controllers/job.js
@@ -2,58 +2,86 @@
 const JobModel = require('../models/job');
 
 const createJob = async (req,res) => {
-    const jobObj = req.body;
-    const newJob = new JobModel(jobObj);//document
-    const newlySavedJob = await newJob.save();//saving document in collection
-    console.log(newlySavedJob);
-
-    res.json({
-        success: true,
-        message: 'Job created successfully',
-        jobId: newlySavedJob._id,
-    })
+    try {
+        const jobObj = req.body;
+        const newJob = new JobModel(jobObj);//document
+        const newlySavedJob = await newJob.save();//saving document in collection
+        console.log(newlySavedJob);
+    
+        res.json({
+            success: true,
+            message: 'Job created successfully',
+            jobId: newlySavedJob._id,
+        })
+    } catch (err) {
+        res.json({
+            success: false,
+            message: "Something went wrong, please try again after sometome",
+        })
+    }
 }
 
 const listJob = async (req,res) => {
     // const minSalary = req.query.minSalary;
     // const maxSalary = req.query.maxSalary;
-    const { minSalary, maxSalary } = req.query;
-    const jobsList = await JobModel.find({
-        $and: [ { salary: { $gte: minSalary }}, { salary: { $lte: maxSalary } } ], 
-    });
-    res.json({
-        success: true,
-        message: 'List job api',
-        results: jobsList,
-    })
+    try {
+        const { minSalary, maxSalary } = req.query;
+        const jobsList = await JobModel.find({
+            $and: [ { salary: { $gte: minSalary }}, { salary: { $lte: maxSalary } } ], 
+        });
+        res.json({
+            success: true,
+            message: 'List job api',
+            results: jobsList,
+        })
+    } catch (err) {
+        res.json({
+            success: false,
+            message: "Something went wrong, please try again after sometome",
+        })
+    }
 }
 
 const editJob = async (req,res) => {
-    const jobId = req.params.id;
+    try {
+        const jobId = req.params.id;
+        
+        // Model.findByIdAndUpdate(_ID TO FIND THE RECORD, FIELDS WITH DATA TO UPDATE)
+        await JobModel.findByIdAndUpdate(jobId, req.body); 
     
-    // Model.findByIdAndUpdate(_ID TO FIND THE RECORD, FIELDS WITH DATA TO UPDATE)
-    await JobModel.findByIdAndUpdate(jobId, req.body); 
-
-    // const findObj = { title: 'Updated Software Engineer' };
-    // const updateObj = { location: 'NY' };
-    // await JobModel.findOneAndUpdate(findObj, updateObj); // updates first matching record
-    // await JobModel.updateMany(findObj, updateObj); // updates all the matching records
-
-    res.json({
-        success: true,
-        message: 'Job edited successfully!'
-    })
+        // const findObj = { title: 'Updated Software Engineer' };
+        // const updateObj = { location: 'NY' };
+        // await JobModel.findOneAndUpdate(findObj, updateObj); // updates first matching record
+        // await JobModel.updateMany(findObj, updateObj); // updates all the matching records
+    
+        res.json({
+            success: true,
+            message: 'Job edited successfully!'
+        })
+    } catch (err) {
+        res.json({
+            success: false,
+            message: "Something went wrong, please try again after sometome",
+        })
+    }
 }
 
 const deleteJob = async (req,res) => {
-    const jobId = req.params.id;
-    await JobModel.findByIdAndDelete(jobId);
-    // JobModel.findOneAndDelete();
-    // JobModel.deleteMany();
-    res.json({
-        success: true,
-        message: 'Dummy delete job api'
-    })
+    try {
+        const jobId = req.params.id;
+        await JobModel.findByIdAndDelete(jobId);
+        // JobModel.findOneAndDelete();
+        // JobModel.deleteMany();
+        res.json({
+            success: true,
+            message: 'Dummy delete job api'
+        })
+    } catch (err) {
+        res.json({
+            success: false,
+            message: "Something went wrong, please try again after sometome",
+        })
+    }
 }
 
 const jobController = {
--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(express.json());
 // MongoDB Connection
 mongoose
 // .connect('mongodb://localhost:27017/jobApp')
-.connect('mongodb://0.0.0.0:27017/jobApp')
+// .connect('mongodb://0.0.0.0:27017/jobApp')
+.connect('mongodb+srv://admin-rachit:a4jKVhconS4k2nkW@jobapp.iojaaqn.mongodb.net/')
 .then(() => console.log("Connection with database established successfully"))
 .catch((err) => console.log('ERROR CONNECTING WITH DATABASE', err));
 
